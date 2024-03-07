e946298170af619e3d2b69bc34a7272e1ede78bd
thomasNotTomas <thomasnottomas@gmail.com>
2024-03-06 23:28:13 -0300

    Service area completed

--- src/components/sections/Areas.jsx
+++ src/components/sections/Areas.jsx
@@ -9,7 +9,7 @@ function Areas() {
         <div className="max-w-7xl mx-auto pb-96">
             <h2 className="text-2xl text-brand-900 font-semibold">AREAS EN LAS QUE TRABAJAMOS</h2>
             <Bar></Bar>
-            <div className="space-y-2 text-white text-lg">
+            <div className="space-y-2 text-white pb-12 text-lg">
                 <div className="bg-brand-700 py-4 px-8">Brindamos productos y servicios movidos por la filosofia de ser una empresa para empresas.</div>
                 <div className="space-y-2 md:space-y-0 md:space-x-2 md:flex">
                 <PrimaryBanner text='Agro' background='bg-[url("/banners/agro-banner.png")]'></PrimaryBanner>
@@ -21,6 +21,14 @@ function Areas() {
                 <PrimaryBanner text='Industria' background='bg-[url("/banners/industry-banner.png")]'></PrimaryBanner>
                 </div>
                 <div className="bg-brand-700 py-4 px-8">Nos encargamos de proveer repuestos y reparaciones de partes para  vehiculos motores maquinas y componentes de instalacion de industrias.</div>
+            </div>
+            <h2 className="text-2xl text-brand-900 font-semibold">ALGUNOS SERVICIOS QUE OFRECEMOS SON</h2>
+            <Bar></Bar>
+            <div className="space-y-2 md:space-y-0 md:space-x-2 md:flex">
+            <PrimaryBanner text='Reparación de bombas hidráulicas' background='bg-[url("/banners/pump-banner.png")]'></PrimaryBanner>
+            <PrimaryBanner text='Reparación y fabricación de cilindros hidráulicos' background='bg-[url("/banners/hydraulic-banner.png")]'></PrimaryBanner>
+            <PrimaryBanner text='Turbos' background='bg-[url("/banners/turbo-banner.png")]'></PrimaryBanner>
+            <PrimaryBanner text='Transmisiones' background='bg-[url("/banners/gear-banner.png")]'></PrimaryBanner>
             </div>
         </div>
 
